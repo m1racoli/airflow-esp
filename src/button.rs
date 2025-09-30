ed7c96763e89fb5532138ef48d9a574711765a3c
Cedrik Neumann <7921017+m1racoli@users.noreply.github.com>
2025-09-30 11:33:33 -0600

    feat: implement DAG to set LED based on button pressed

--- src/button.rs
+++ src/button.rs
@@ -1,11 +1,16 @@
-use embassy_sync::pubsub::PubSubBehavior;
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex, pubsub::PubSubBehavior, signal::Signal,
+};
 use embassy_time::{Duration, Timer};
 use esp_hal::gpio::Input;
+use serde::{Deserialize, Serialize};
 use tracing::debug;
 
 use crate::{EVENTS, Event};
 
-#[derive(Debug, Clone, Copy)]
+static NEXT_BUTTON_PRESSED: Signal<CriticalSectionRawMutex, Button> = Signal::new();
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum Button {
     Boot,
     Green,
@@ -23,6 +28,7 @@ async fn _listen_button(mut input: Input<'_>, button: Button) {
         input.wait_for_falling_edge().await;
         debug!("Button {:?} pressed", button);
         EVENTS.publish_immediate(Event::ButtonPressed(button));
+        NEXT_BUTTON_PRESSED.signal(button);
         debounce_button(&mut input).await;
     }
 }
@@ -33,4 +39,10 @@ async fn debounce_button(input: &mut Input<'_>) {
         input.wait_for_rising_edge().await;
         Timer::after(Duration::from_millis(100)).await;
     }
+}
+
+pub async fn next_button_pressed() -> Button {
+    // reset the signal to ensure we wait for a new button press
+    NEXT_BUTTON_PRESSED.reset();
+    NEXT_BUTTON_PRESSED.wait().await
 }
--- src/example.rs
+++ src/example.rs
@@ -1,123 +1,68 @@
 use airflow_task_sdk::prelude::*;
-use alloc::string::{String, ToString};
-use core::{f32, time::Duration};
-use serde_json::{Value, json};
-use tracing::{info, warn};
+use tracing::info;
 
-#[derive(Debug, Clone)]
-pub struct ExampleOperator {
-    sleep: Duration,
-}
+use crate::{
+    button::{Button, next_button_pressed},
+    led::{Led, set_led},
+};
 
-impl ExampleOperator {
-    pub fn new(sleep_secs: u64) -> Self {
-        Self {
-            sleep: Duration::from_secs(sleep_secs),
-        }
-    }
-}
+#[derive(Debug, Clone, Default)]
+pub struct ButtonSensor;
 
-impl<R: TaskRuntime> Operator<R> for ExampleOperator {
-    type Output = Value;
+impl<R: TaskRuntime> Operator<R> for ButtonSensor {
+    // Button implements serde::Serialize and serde::Deserialize
+    type Output = Button;
 
-    fn set_opts(&self, opts: &mut OperatorOpts) {
-        opts.multiple_outputs = true;
-    }
-
-    async fn execute<'t>(&'t mut self, ctx: &'t Context<'t, R>) -> Result<Self::Output, TaskError> {
-        info!(
-            dag_id = %ctx.dag_id(),
-            task_id = %ctx.task_id(),
-            run_id = %ctx.run_id(),
-            try_number = %ctx.try_number(),
-            map_index = %ctx.map_index(),
-            "I am a task instance!",
-        );
-
-        ctx.task_instance().xcom_push("example_key", &42).await?;
-
-        R::sleep(self.sleep).await;
-
-        warn!("This feels very fast! 😎");
-        info!("I am done");
-
-        let output = json!({
-            "pi": f32::consts::PI,
-            "list": [1, 2, 3],
-            "map": { "hello": "world" }
-        });
-        Ok(output)
+    async fn execute<'t>(
+        &'t mut self,
+        _ctx: &'t Context<'t, R>,
+    ) -> Result<Self::Output, TaskError> {
+        info!("Waiting for button press...");
+        let button = next_button_pressed().await;
+        info!("Button {:?} pressed", button);
+        Ok(button)
     }
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct PrintXComOperator {
-    task_id: String,
-}
+pub struct LedOperator;
 
-impl PrintXComOperator {
-    pub fn new(task_id: &str) -> Self {
-        Self {
-            task_id: task_id.to_string(),
-        }
-    }
-}
-
-impl<R: TaskRuntime> Operator<R> for PrintXComOperator {
+impl<R: TaskRuntime> Operator<R> for LedOperator {
     type Output = ();
 
     async fn execute<'t>(&'t mut self, ctx: &'t Context<'t, R>) -> Result<Self::Output, TaskError> {
-        let ti = ctx.task_instance();
-
-        let pi: f32 = ti
+        let button: Button = ctx
+            .task_instance()
             .xcom_pull()
-            .task_id(&self.task_id)
-            .key("pi")
+            .task_id("wait_button")
             .one()
             .await?;
-        info!("PI is {}", pi);
 
-        // Use Option to handle missing XCom values
-        let non_existing: Option<String> = ti
-            .xcom_pull()
-            .task_id(&self.task_id)
-            .key("non_existing")
-            .one()
-            .await?;
-        assert_eq!(non_existing, None);
+        info!("Got button {:?} from upstream task", button);
+
+        let led = match button {
+            Button::Green => Led::Green,
+            Button::Yellow => Led::Yellow,
+            Button::Red => Led::Red,
+            Button::Boot => Led::None,
+        };
+
+        info!("Setting LED to {:?}", led);
+        set_led(led);
 
         Ok(())
     }
 }
 
-#[derive(Debug, Clone, Default)]
-pub struct RetryOperator;
-
-impl<R: TaskRuntime> Operator<R> for RetryOperator {
-    type Output = ();
-
-    async fn execute<'t>(&'t mut self, ctx: &'t Context<'t, R>) -> Result<Self::Output, TaskError> {
-        if ctx.try_number() < 2 {
-            info!("Failing task, attempt {}", ctx.try_number());
-            Err(TaskError::Unknown("Failing task".to_string()))
-        } else {
-            info!("Succeeding task, attempt {}", ctx.try_number());
-            Ok(())
-        }
-    }
-}
-
 pub fn get_dag_bag<R: TaskRuntime>() -> DagBag<R> {
-    let run = ExampleOperator::new(5).into_task("run");
-    let print_xcom = PrintXComOperator::new("run").into_task("print_xcom");
-    let retry = RetryOperator.into_task("retry");
+    let wait_button = ButtonSensor.into_task("wait_button");
+    let set_led = LedOperator.into_task("set_led");
 
-    let mut dag = Dag::new("example_dag");
-    dag.add_task(run);
-    dag.add_task(print_xcom);
-    dag.add_task(retry);
+    let mut airflow_summit_demo = Dag::new("airflow_summit_demo");
+    airflow_summit_demo.add_task(wait_button);
+    airflow_summit_demo.add_task(set_led);
 
     let mut dag_bag = DagBag::default();
-    dag_bag.add_dag(dag);
+    dag_bag.add_dag(airflow_summit_demo);
     dag_bag
 }
